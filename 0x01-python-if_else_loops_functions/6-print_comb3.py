97cacd3abce1c13362caa166c06bb99f98cf1d89
jhay3rd <johnsonibe0@gmail.com>
2023-11-28 07:51:21 -0800

    if, else, loops 3.0

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-for i in range(9):
-for j in range(i + 1, 10):
-
-if i * 10 + j < 89:
-print("{:d}{:d}".format(i, j), end=", ")
+for number in range(9):
+    for j in range(number + 1, 10):
+        if number * 10 + j < 89:
+            print("{:d}{:d}".format(num, j), end=", ")
 print("{:d}".format(89))
